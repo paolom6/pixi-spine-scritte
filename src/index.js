341fc59b364b937ecf05afc2c124677db9e099c8
paolom6 <129654380+paolom6@users.noreply.github.com>
2024-06-28 15:21:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import "./styles.css";
 import * as PIXI from "pixi.js";
-import "pixi-spine";
 import { Spine } from "pixi-spine";
 const fs = require("fs");
 var files = fs.readdirSync("/assets/dragon");
@@ -9,6 +8,7 @@ var path = require("path");
 for (var i in files) {
   if (path.extname(files[i]) === ".json") {
     var directory = "assets/dragon/" + files[i];
+    break;
   }
 }
 
@@ -20,14 +20,17 @@ document.body.appendChild(app.view);
 const array = Object.keys(data.animations);
 console.log(array);
 
-// load spine data
 PIXI.Assets.load(directory).then((resource) => {
   const dragon = new Spine(resource.spineData);
   var animation = 0.0;
+  dragon.state.timeScale = 1.0;
   const style = new PIXI.TextStyle({
     fill: ["#ffffff", "#ffffff"],
   });
-  const text = new PIXI.Text(array[0], style);
+  const text = new PIXI.Text(
+    array[0] + " velocità: " + dragon.state.timeScale + "x",
+    style
+  );
   dragon.state.setAnimation(0, array[0], true);
   for (let i = 0; i < 1; i++) {
     document.addEventListener("keydown", function (event) {
@@ -35,14 +38,34 @@ PIXI.Assets.load(directory).then((resource) => {
         animation = animation - 1;
         console.clear();
         console.log(array[animation]);
-        text.text = array[animation];
+        text.text =
+          array[animation] +
+          " velocità: " +
+          dragon.state.timeScale.toFixed(1) +
+          "x";
         dragon.state.setAnimation(0, array[animation], true);
       } else if (event.key == "ArrowRight" && animation < array.length - 1) {
         animation = animation + 1;
-        console.clear();
-        console.log(array[animation]);
-        text.text = array[animation];
+        text.text =
+          array[animation] +
+          " velocità: " +
+          dragon.state.timeScale.toFixed(1) +
+          "x";
         dragon.state.setAnimation(0, array[animation], true);
+      } else if (event.key == "ArrowUp" && dragon.state.timeScale < 3) {
+        dragon.state.timeScale = dragon.state.timeScale + 0.3;
+        text.text =
+          array[animation] +
+          " velocità: " +
+          dragon.state.timeScale.toFixed(1) +
+          "x";
+      } else if (event.key == "ArrowDown" && dragon.state.timeScale > 0.2) {
+        dragon.state.timeScale = dragon.state.timeScale - 0.3;
+        text.text =
+          array[animation] +
+          " velocità: " +
+          dragon.state.timeScale.toFixed(1) +
+          "x";
       }
     });
   }
